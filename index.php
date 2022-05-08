4643929b6a283dca1d84ebfd6b5bf7e6a0d0442d
minh <ldm20042000@gmail.com>
2022-05-08 21:45:16 +0700

    make mvc

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-    echo $_GET["url"];
+session_start();
+
+spl_autoload_register(function ($class){
+    $root = dirname(__FILE__);
+    $file = $root . '/' . str_replace("\\",'/',$class) . '.php';
+    if (is_readable($file)) {
+        require $file;
+    }
+});
+
+$router = new Core\Router();
+
+$router->add('',['controller'=>'Home','action'=>'index','namespace'=>'Home']);
+$router->add("{controller}/{action}",['namespace'=>'Home']);
+// $router->add("{controller}/{action}");
+// $router->add("admin/{controller}/{action}",['namespace'=>'Admin']);
+// $router->add("{controller}/{id:\d+}/{action}");
+
+
+$router->dispatch($_SERVER["QUERY_STRING"]);
+
+
 
 ?>
